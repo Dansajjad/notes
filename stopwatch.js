3fee8df9cd145de194a2e8c037450b0186460ad1
Hack Reactor Students <accounts@hackreactor.com>
2016-04-19 09:32:49 -0700

    Created a stopwatch

---
+++ stopwatch.js
@@ -0,0 +1,16 @@
+//Closures
+
+var makestopwatch = function() {
+    var elapsed = 0; 
+    var stopwatch = function() { return elapsed; };
+    var increment = function() { elapsed++; };
+    setInterval(increment, 1000); 
+    return stopwatch;
+}
+
+var stopwatch3 = makestopwatch();
+
+var stopwatch4 = makestopwatch();
+
+console.log(stopwatch3());
+console.log(stopwatch4());
